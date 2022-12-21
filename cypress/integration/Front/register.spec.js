84306186f7d84f7937c1567bd41b67645ab80147
eu-MicheleOliveira <michele0131@outlook.com>
2022-12-20 21:35:55 -0300

    update test

--- cypress/integration/Front/register.spec.js
+++ cypress/integration/Front/register.spec.js
@@ -3,7 +3,7 @@ import locators from '..\support\locators'
 
 describe('To register on demo site', function () {
 
-        it.only('to register new user', function () {
+        it('to register new user', function () {
             cy.visit('https://alura-fotos.herokuapp.com/#/home')
             cy.contains('Login').should('be.visible')
 
